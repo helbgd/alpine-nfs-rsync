cbea5871ded208ebfcfe1960fb87f7667b254479
helbgd <helmut.angerer@googlemail.com>
2016-05-13 23:00:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,37 +6,17 @@ apk update && apk upgrade
 # we need the real (GNU) wget, the one incl. with Alpine's busybox is lacking some options
 # install python for 
 apk add wget
-apk add python
+apk add rsync
 
-# add a non-root user and group called "nzbget" with no password, no home dir, no shell, and gid/uid set to 1000
-addgroup -g 1000 nzbget && adduser -H -D -G nzbget -s /bin/false -u 1000 nzbget
+# add a non-root user and group called "rsync" with no password, no home dir, no shell, and gid/uid set to 1000
+addgroup -g 1000 rsync && adduser -H -D -G rsync -s /bin/false -u 1000 rsync
 
-# create the install dir and volume mount points
-mkdir /nzbget && mkdir /config && mkdir /downloads
-
-# download the testing nzbget version
-wget -O - http://nzbget.net/info/nzbget-version-linux.json | \
-sed -n "s/^.*testing-download.*: \"\(.*\)\".*/\1/p" | \
-wget --no-check-certificate -i - -O /setup/nzbget-testing-bin-linux.run || \
-echo "*** Download failed ***"
-
-# let's install nzbget (defaults to the "/nzbget" directory)
-sh /setup/nzbget-testing-bin-linux.run --destdir /nzbget
-
-# check for and modify the config for our container
-sh /setup/modify_config_for_container_env.sh
-
-# download https://raw.githubusercontent.com/cytec/nzbget-scripts/master/Scan/NZBPass.py
-wget https://raw.githubusercontent.com/cytec/nzbget-scripts/master/Scan/NZBPass.py --no-check-certificate -i - -O /nzbget/scripts/NZBPass.py
-chmod a+x /nzbget/scripts/NZBPass.py
-
-# change the owner accordingly
-chown -R nzbget:nzbget /nzbget /config /downloads /setup/nzbget-testing-bin-linux.run
-
+# create the rsync dir and volume mount points
+mkdir /export
 
 
 # we don't need GNU wget anymore (busybox' wget will still be available).
-apk del wget
+#apk del wget
 
 # also, clear the apk cache:
-rm -rf /var/cache/apk/*
+#rm -rf /var/cache/apk/*
